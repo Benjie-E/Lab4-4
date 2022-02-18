9a0b535767124b1a1ffd147ffe67f36c81887042
Benjie-E <90060865+Benjie-E@users.noreply.github.com>
2022-02-18 15:11:55 -0500

    Cert of Auth

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
 /*
- * main.cpp
- *
- *  Created on: Feb 8, 2022
- *      Author: Benjie
- */
+Author:       Benjie
+Class:        CSI-240-03
+Assignment:   Lab 4-4
+Date Assigned:2/7/2022
+Due Date:     2/22/2022 14:30
+Description:
+Bubble sort on array
+Certification of Authenticity:
+    I certify that this is entirely my own work, except where I have given
+    fully-documented references to the work of others. I understand the
+    definition and consequences of plagiarism and acknowledge that the assessor
+    of this assignment may, for the purpose of assessing this assignment:
+        - Reproduce this assignment and provide a copy to another member of
+academic staff; and/or
+        - Communicate a copy of this assignment to a plagiarism checking
+          service (which may then retain a copy of this assignment on its
+          database for the purpose of future plagiarism checking)
+*/
 #include <iostream>
 #include <string>
 #include <array>
@@ -13,8 +26,6 @@
 const int UPPER_BOUND = 51; // The largest random number, inclusive
 
 using namespace std;
-static int count = 0;
-static int total=0;
 void fillArray(int *pArray, int size) {
 
 	for (int i = 0; i < size; i++) {
@@ -37,7 +48,7 @@ void sortArray(int *pArray, int size) {
 	do{
 		test=true;
 	for(int i=0;i<size-1;i++){
-		if(pArray[i]>pArray[i+1]){count++;
+		if(pArray[i]>pArray[i+1]){
 			buffer=pArray[i];
 			pArray[i]=pArray[i+1];
 			pArray[i+1]=buffer;
@@ -46,23 +57,19 @@ void sortArray(int *pArray, int size) {
 }
 int main() {
 	srand(time(NULL));
-	for(int j=0;j<100;j++){
-	int size=100;
-//	do {
-//		cout << "How big an array:";
-//		cin >> size;
-//	} while (size < 1);
+	int size;
+	do {
+		cout << "How big an array:";
+		cin >> size;
+	} while (size < 1);
 	int *array = new int[size];
 	fillArray(array, size);
-	printArray(array, size);
+
+	printArray(array,size);
 	cout << endl;
 	sortArray(array,size);
-	//printArray(array, size);
+	printArray(array, size);
 	delete array;
-	//printArray(array,size);
-	cout << endl<< count;
-	total+=count;
-	count=0;
-}cout << total<<endl;
-cout << total/100;}
+	array=nullptr;
+}
 
